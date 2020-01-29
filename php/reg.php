7ccf99bfbabb3afbb7b9a91db7f873b470f54522
demenkov_as <pochta4rabota@gmail.com>
2020-01-29 16:58:12 +0300

    add reg.php & eslint

---
+++ php/reg.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../style.css">
+    <title>Registation on drumJS</title>
+</head>
+<body>
+    <hr>
+    <form action="reg.php">
+        <label for="">Name</label>
+        <input type="text">
+        <label for="">E-mail</label>
+        <input type="email">
+        <label for="">Password</label>
+        <input type="password">
+        <input type="submit">
+        <input type="reset">
+    </form>
+</body>
+</html>
